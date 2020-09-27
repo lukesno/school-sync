7365e692728b2620724bb880c0497224117d8f74
Andrew Ma <andrew.ma1@cibc.com>
2020-09-26 22:22:56 -0600

    Working copy of assignments list page. Made individual assignment components

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,23 +1,108 @@
 import styles from './Dashboard.style';
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, SectionList } from 'react-native';
 import fire from '../fire';
 import AddAssignmentButton from './AddAssignmentButton';
+import { Container } from 'native-base';
+
+//for testing with json format data
+// const DATA = [
+//     {
+//         title: "Math Problem Set 3",
+//         date: "Sept. 30, 2020",
+//         course: "123",
+//         description: "Math problem set is going to be very hard this week. Make sure to attend office hours if you need help!"
+//     },
+//     {
+//         title: "Chemistry Write Up Due",
+//         date: "Sept. 28, 2020",
+//         course: "351",
+//         description: "Better have your ten page writeup finished lmao",
+//     }
+
+// ]
+
+const Assignment = ({ date, title, course}) => {
+    return(
+        <View style={styles.assignmentContainer}> 
+            <View style={{width: '75%'}}>
+                <Text style={styles.assignmentTitleText}>{title}</Text>
+            </View>
+            <View style={{marginTop: 3}}>
+                <Text style={styles.assignmentDateText}>{date}</Text>
+            </View>
+        </View>
+    )
+}
 
 const Dashboard = ({user, navigation}) => {
     // Take in some props; in order to access data accordingly in Firebase
     console.log(user)
     return (
-        <View>
-            <Text>Abc</Text>
+        <Container>
+            {/* <SectionList
+                sections={DATA}
+                keyExtractor={( item, index ) => item + index}
+                renderItem={({ item }) => <Assignment title={item} />}
+            >
+
+            </SectionList> */}
+            <View style={styles.headerContainer}>
+                <Text style={styles.screenTitleText}>
+                    Assignments
+                </Text>
+                <View style={styles.screenDivider}>
+                    <Text style={styles.screenDividerText}>
+                        Upcoming
+                    </Text>
+                </View>
+            </View>
+            <View
+                style={styles.assignmentListContainer}
+            >
+                <Assignment     
+                    title={"Math Problem Set 3" }
+                    date={"Sept. 30, 2020"}
+                />    
+                <Assignment
+                    title={"Chemistry Write Up"}
+                    date={"Oct. 4, 2020"}
+                />
+                <Assignment
+                    title={"English Essay Due"}
+                    date={"Oct. 4, 2020"}
+                />
+                <Assignment
+                    title={"Engineering Phsyics Assignment"}
+                    date={"Oct. 4, 2020"}
+                />
+                <Assignment
+                    title={"Unit Mindmap Due"}
+                    date={"Oct. 4, 2020"}
+                />
+                <Assignment
+                    title={"SOMA Survey Deadline"}
+                    date={"Oct. 4, 2020"}
+                />
+                <Assignment
+                    title={"Another Assignment"}
+                    date={"Oct. 4, 2020"}
+                />
+                <Assignment
+                    title={"Linear Algebra Quiz"}
+                    date={"Oct. 4, 2020"}
+                />
+            </View>
+            <View style={{  flex: 1 }}>
+                <AddAssignmentButton onPress={() => navigation.navigate("Enroll")}/>
+            </View>
             <TouchableOpacity style={styles.signOutButton} onPress={() => {
                 fire.auth().signOut().then(() => alert('User signed out!'));
                 navigation.navigate('Main')
             }}>
                 <Text style={styles.signOutText}>Sign Out</Text>
             </TouchableOpacity>
-            <AddAssignmentButton/>
-        </View>
+        </Container>
     );
 }
 
--- components/Dashboard.style.js
+++ components/Dashboard.style.js
@@ -1,11 +1,56 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    headerContainer: {
+        marginTop: 60,
+    },
+    screenTitleText: {
+        textAlign: 'center',
+        fontFamily: 'Verdana',
+        fontSize: 18,
+        fontWeight: '500',
+        color: '#454545'
+    },
+    screenDivider: {
+        height: 30,
+        marginTop: 10,
+        backgroundColor: '#e3ebff',
+        width: '100%',
+    },
+    screenDividerText: {
+        padding: 6,
+        paddingLeft: 20,
+        color: '#7a7a7a',
+        fontWeight: '400'
+    },
+    assignmentContainer: {
+        // backgroundColor: 'green',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 25,
+        alignItems: 'flex-start',
+    },
+    assignmentListContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: 5,
+        // backgroundColor: 'blue'
+    },
+    assignmentTitleText: {
+        fontSize: 15,
+    },
+    assignmentDateText: {
+        fontSize: 12,
+        color: "#737272"
+    },
     signOutButton: {
         // Add styling
         alignItems: 'center',
         justifyContent: 'center',
-        paddingHorizontal: 20,
+        paddingHorizontal: 0,
         paddingVertical: 10,
         backgroundColor: "#333287",
         borderRadius: 5,
@@ -18,7 +63,7 @@ const styles = StyleSheet.create({
         color: 'white',
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
 });
 
 export default styles;
